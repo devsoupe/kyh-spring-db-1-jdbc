d8a7a2830e8032be04fdea9e20c7092c089eac22
devsoupe <luvyum4e@gmail.com>
2023-06-10 16:55:31 +0900

    first commit

---
+++ src/test/java/com/hello/jdbc/KyhSpringDb1JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhSpringDb1JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
